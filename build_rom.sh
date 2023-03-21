f37c3a229d30363e158bb04cecffba1fb3325404
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-03-21 14:11:49 +0700

    X01AD-BootleggersROM-tirimbino-Assunzain

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build roms
 source build/envsetup.sh
 lunch bootleg_X01AD-userdebug
+export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_USERNAME=assunzain
 export BUILD_HOSTNAME=zain
 export TZ=Asia/Jakarta #put before last build command
