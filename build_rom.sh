aa47da0cda0280b520857adc3598a38c1ab54822
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-03-22 11:11:27 +0700

    X01AD-DerpFest-AOSP-13-Assunzain
    X01AD-DerpFest-AOSP-13-Assunzain

--- build_rom.sh
+++ build_rom.sh
@@ -1,18 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/BootleggersROM/manifest.git -b tirimbino -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Assunzain/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build roms
+# build rom
 source build/envsetup.sh
-lunch bootleg_X01AD-userdebug
-export ALLOW_MISSING_DEPENDENCIES=true
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-export BUILD_USERNAME=assunzain
-export BUILD_HOSTNAME=zain
+lunch derp_X01AD-userdebug
+export BUILD_USERNAME=AssunZain
+export BUILD_HOSTNAME=AssunZain
 export TZ=Asia/Jakarta #put before last build command
-mka bacon
+mka derp
 
-
-## upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
