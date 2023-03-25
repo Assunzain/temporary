cfb48860a21370bf982bc2d7f89794e895b77c7e
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-03-25 07:50:36 +0700

    X01AD-Project-Xtended-xt-Assunzain

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Project-Xtended/manifest.git -b xt -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Assunzain/local_manifest.git --depth 1 -b ProjectX .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 -j1 --fail-fast
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
  
 # build rom
 source build/envsetup.sh
