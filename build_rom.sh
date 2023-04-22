08ef03ef2d3f24266a339f92c4b8c6ab98e13e08
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-04-23 01:03:12 +0700

    X01AD-Project-Xtended-xt-Assunzain

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Project-Xtended/manif
 git clone https://github.com/Assunzain/local_manifest -b ProjectX .repo/local_manifests  
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8  
   
-# build rom # 
+# build rom
 source build/envsetup.sh  
 lunch xtended_X01AD-userdebug  
 export TZ=Asia/Jakarta #put before last build command  
