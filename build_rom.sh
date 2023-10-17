455ed5f9f6d6b0ecf5a83d056a41694d7eda42b2
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-10-18 05:54:59 +0700

    X01AD-alphadroid-project-alpha-13-Assunzain

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init -u https://github.com/alphadroid-project/manifest -b alpha-13 --git-lfs -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/manifest -b alpha-13 --git-lfs -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Assunzain/local_manifest -b alpha .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
