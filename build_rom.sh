84c1e95a9f1a812d72851821d0acd21b88305eaa
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-10-20 06:37:53 +0700

    X01AD-CherishOS-tiramisu-Assunzain

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
- # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Assunzain/local_manifest -b Derp .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+# sync rom
+repo init -u https://github.com/CherishOS/android_manifest.git -b tiramisu
+git clone https://github.com/Assunzain/local_manifest -b chiper .repo/local_manifests
+repo sync -c -j$(nproc --all) --no-clone-bundle
 
-# build rom 
-source build/envsetup.sh
-lunch derp_X01AD-userdebug
+# build rom
+. build/envsetup.sh
+lunch cherish_X01AD-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Jakarta #put before last build command
-mka derp
+make bacon -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
